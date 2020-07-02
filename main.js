d04ec3902989335738c1bd00986304bfa74f0947
Andrea <andreapirrello1990@gmail.com>
2020-07-02 16:18:18 +0200

    impostato esercizio

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+
+// promemoria mio
+
+// $(selector).action(); sintassi base JQuery
+
+// in versione mobile devo far in modo che al cliccare sulle barre il menu compaia a cascata
+
+
+$(".fas").click(function()) {
+
+
+}
